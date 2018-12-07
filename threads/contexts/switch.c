9d29199765ee7dcccbe8b117a7690f450f22b3f8
Louis Ubuntu <me@louiscb.com>
2018-12-07 17:42:07 +0100

    Added thread folder

---
+++ threads/contexts/switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ucontext.h>
+
+int mainjkll() {
+    //making done a register will result in a infinite loop
+    int done = 0;
+    ucontext_t one;
+    ucontext_t two;
+    ucontext_t three;
+
+    //saves current state of program in context one
+    getcontext(&one);
+
+    printf("Hello, World!\n");
+
+    if (!done) {
+        done = 1;
+        //saves current context in two
+        //executes from saved context one
+        swapcontext(&two, &one);
+
+        printf("We should not see this...");
+    }
+
+    return 0;
+}
