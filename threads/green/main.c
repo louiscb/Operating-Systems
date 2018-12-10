cd99777aa490a267fe3e4ffdd00cf711100569fc
Louis <me@louiscb.com>
2018-12-10 15:05:41 +0100

    Cleaned up main

--- threads/green/main.c
+++ threads/green/main.c
@@ -4,13 +4,18 @@
 #include "green.h"
 #include <pthread.h>
 
+int a0 = 0;
+int a1 = 1;
+int a2 = 2;
+int a3 = 3;
+int TOTAL;
+
 void *test(void *arg) {
     int i = *(int*)arg;
-    int loop = 4;
+    int loop = TOTAL;
 
     while (loop > -1) {
         printf("Thread: %d - %*s %d\n", i, loop, " ", loop);
-        //printf("thread %d: %d%*s\n", i, loop, loop);
         loop--;
         green_yield();
     }
@@ -18,46 +23,52 @@ void *test(void *arg) {
 
 void *test2(void *arg) {
     int i = *(int*)arg;
-    int loop = 4;
+    int loop = TOTAL;
 
     while (loop > -1) {
         printf("Thread: %d - %*s %d\n", i, loop, " ", loop);
-        //printf("thread %d: %d%*s\n", i, loop, loop);
         loop--;
-       // pthread_yield();
+        pthread_yield();
     }
 }
 
 int main() {
     printf(" - START -\n");
-    green_t g0, g1, g2, g3;
-   // pthread_t p0, p1, p2, p3;
-    int a0 = 0;
-    int a1 = 1;
-    int a2 = 2;
-    int a3 = 3;
 
-    green_create(&g0, test, &a0);
-    green_create(&g1, test, &a1);
-    green_create(&g2, test, &a2);
-//    green_create(&g3, test, &a3);
-
-    //  printf(" - JOINING 1 - \n");
-   green_join(&g0);
-   // printf(" - JOINING 2 - \n");
-   green_join(&g1);
-    green_join(&g2);
-
-    //printf("\nREAL SHIT\n");
-
-    //pthread_create(&p0, NULL, test2, &a0);
-    //pthread_create(&p1, NULL, test2, &a1);
-   // pthread_create(&p2, NULL, test, &a2);
-    //pthread_create(&p3, NULL, test, &a3);
-
-   // pthread_join(p0, NULL);
+    greenTest();
+    pthreadTest();
 
     printf("- END -\n");
 
     return 0;
+}
+
+void greenTest() {
+    printf("-- Running Green Threading --\n");
+    green_t g0, g1, g2;
+
+    green_create(&g0, test, &a0);
+    green_create(&g1, test, &a1);
+    green_create(&g2, test, &a2);
+
+    green_join(&g0);
+    green_join(&g1);
+    green_join(&g2);
+    printf("-- Ending Green Threading --\n");
+    return;
+}
+
+void pthreadTest() {
+    printf("-- Running Pthread --\n")
+    pthread_t p0, p1, p2, p3;
+
+    pthread_create(&p0, NULL, test2, &a0);
+    pthread_create(&p1, NULL, test2, &a1);
+    pthread_create(&p2, NULL, test2, &a0);
+
+    pthread_join(p0, NULL);
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+    printf("-- Ending Pthread --\n");
+    return;
 }
