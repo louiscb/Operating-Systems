ff4c4565548fce870c173ec7a20efc2dd4ab983e
Louis <me@louiscb.com>
2019-01-30 11:40:28 +0100

    reorganised files

---
+++ multi-threading/queue.h
@@ -0,0 +1,15 @@
+//
+// Created by el-plate on 2018-12-10.
+//
+
+#ifndef GREEN_QUEUE_H
+#define GREEN_QUEUE_H
+#include "common.h"
+
+void enqueue(queue *queue1, green_t *thread);
+
+green_t *dequeue(queue *queue1);
+
+void debugQueue(queue *queue1);
+
+#endif //GREEN_QUEUE_H
